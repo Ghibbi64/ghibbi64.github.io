5638ccdcd0144d7eda88cb9f3dd8551c0e421324
Gabriel P <marioghibbi2@gmail.com>
2022-11-05 22:28:17 +0100

    impostazioni + changeBkg + popup in mainpage

---
+++ script.js
@@ -0,0 +1,20 @@
+function showPopup() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  function changeBkgColor() {
+    document.getElementById("body").style.backgroundColor = "#FF0000";
+  }
